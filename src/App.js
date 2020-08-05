38e3f1f065ec5a9b5fb364e8c4a5326b2ff3dfce
Sandra D'Angelo <skdangelo@gmail.com>
2020-08-05 08:41:41 +0000

    Setup note-api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
+import urlFor from './helpers/urlFor';
 
 class App extends Component {
 
@@ -19,13 +21,19 @@ class App extends Component {
   	});
   }
 
+  getNotes = () => {
+  	axios.get(urlFor('notes'))
+  	.then((res) => console.log(res.data) )
+  	.catch((err) => console.log(err.response.data) );
+  }
+
   render() {
   	const { showNote } = this.state;
 
     return (
     <div className="App">
       <Nav toggleNote = {this.toggleNote} showNote={showNote} />
-      { showNote ? <Note /> : <List /> }
+      { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
     </div>
     );
   }
---
+++ src/helpers/urlFor.js
@@ -0,0 +1,5 @@
+const urlFor = (endpoint) => {
+	return 'https://note-api-skdangelo.herokuapp.com/' + endpoint;
+};
+
+export default urlFor;
